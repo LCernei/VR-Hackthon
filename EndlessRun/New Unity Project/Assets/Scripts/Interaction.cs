d5ad036bea13e66db1901e2c9662e0f78ea34735
Eugenius <eugen.cernei@gmail.com>
2018-03-04 00:56:25 +0200

    day2

---
+++ EndlessRun/New Unity Project/Assets/Scripts/Interaction.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class Interaction : MonoBehaviour {
+	public float interval;
+	// Use this for initialization
+	void Start () {
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+		RaycastHit hit = new RaycastHit ();
+		Ray ray = new Ray ();
+		ray = Camera.main.ViewportPointToRay (new Vector3 (0.5f, 0.5f, 1.0f));
+
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.transform.name == "Cube") {
+				if (interval > 0) {
+
+					interval -= Time.deltaTime;
+				} else {
+					//Application.LoadLevel (1);
+					SceneManager.LoadScene (1);
+				}
+			} 
+			if (hit.transform.name == "Capsule") {
+				if (interval > 0) {
+
+					interval -= Time.deltaTime;
+				} else {
+					Application.Quit ();
+				}
+			} 
+		} else {
+			interval = 1;
+		}
+
+
+	}		
+}
---
+++ EndlessRun/New Unity Project/Assets/Scripts/MoveScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour 
+{
+
+	void OnTriggerEnter(Collider other)
+	{	//Debug.Log("colision "+ other.tag);
+		
+		if (other.CompareTag ("Player")) {
+			SceneManager.LoadScene (0);
+			//Application.LoadLevel(0);
+		}
+
+	}
+		
+}
